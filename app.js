33b7301cc32165eec4c3515147cf8efc28a36bdc
Exoper <hchauhan012@gmail.com>
2021-08-18 07:49:34 +0530

    data saved

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app = express();
 app.use(express.static(__dirname+'/public'));
 app.use('/jspsych', express.static(__dirname + "/jspsych"));
 
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({limit: '500mb',extended:false}));
+
 console.log(__dirname);
 
 // --- VIEW LOCATION, SET UP SERVING STATIC HTML
@@ -25,10 +28,23 @@ app.get('/experiment', function(request, response) {
     response.render('experiment.html');
 });
 
+app.get('/endpage',function(request,response){
+	response.render('endpage.html');
+});
 
+app.post('/video_data', function(req,res){
+	console.log(req.headers);
+
+	res.status(200).send({success:true});
+});
+
+app.post('/save_data',function(req,res){
+	//console.log(req.body);
+	res.status(200).send({success:true});
+});
 
 // --- START THE SERVER 
-var server = app.listen(process.env.PORT , function(){
+var server = app.listen(process.env.PORT||3000 , function(){
     console.log("Listening on port %d", server.address().port);
 });
 
